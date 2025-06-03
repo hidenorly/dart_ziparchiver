bb349706e156ce0e56dc7ce82b9997890846f18e
hidenorly <twitte.harold@gmail.com>
2025-06-04 06:28:37 +0900

    Rebase with lib/ziparchiver.dart and the test

--- lib/ziparchiver.dart
+++ lib/ziparchiver.dart
@@ -19,13 +19,19 @@ import 'package:ffi/ffi.dart';
 import 'dart:io';
 import 'dart:isolate';
 import 'dart:typed_data';
+import 'package:path/path.dart' as p;
 
-// load libzip shared library
-final dylib = DynamicLibrary.open(
-    Platform.isMacOS ? "/opt/homebrew/lib/libzip.dylib" :
-    Platform.isLinux ? "libzip.so" :
-    "libzip.dll");
+class IZipArchiver {
+  void open(String zipName, [String? password]){}
+  void addFile(String path, String targetPath) async{}
+  void rename(String oldName, String newName) {}
+  void renameFolder(int zipPointer, String oldFolder, String newFolder) {}
+  void remove(String fileName) {}
+  void close() {}
+}
 
+
+// --- LibZipArchiver
 typedef ZipOpenNative = Pointer<Void> Function(Pointer<Utf8>, Int32, Pointer<Int32>);
 typedef ZipOpenDart = Pointer<Void> Function(Pointer<Utf8>, int, Pointer<Int32>);
 
@@ -74,21 +80,21 @@ typedef ZipGetErrorNative = Pointer<ZipError> Function(Pointer<Void>);
 typedef ZipGetErrorDart = Pointer<ZipError> Function(Pointer<Void>);
 
 
-class ZipArchiver {
-  final zip_open = dylib.lookupFunction<ZipOpenNative, ZipOpenDart>('zip_open');
-  final zip_close = dylib.lookupFunction<ZipCloseNative, ZipCloseDart>('zip_close');
-  final zip_source_buffer = dylib.lookupFunction<ZipSourceBufferNative, ZipSourceBufferDart>('zip_source_buffer');
-  final zip_source_free = dylib.lookupFunction<ZipSourceFreeNative, ZipSourceFreeDart>('zip_source_free');
+class LibZipArchiver extends IZipArchiver {
+  late final zip_open;
+  late final zip_close;
+  late final zip_source_buffer;
+  late final zip_source_free;
 
-  final zip_file_add = dylib.lookupFunction<ZipFileAddNative, ZipFileAddDart>('zip_file_add');
-  final zip_set_file_encryption = dylib.lookupFunction<ZipSetFileEncryptionNative, ZipSetFileEncryptionDart>('zip_file_set_encryption');
-  final zip_file_rename = dylib.lookupFunction<ZipRenameNative, ZipRenameDart>('zip_file_rename');
-  final zip_delete = dylib.lookupFunction<ZipDeleteNative, ZipDeleteDart>('zip_delete');
+  late final zip_file_add;
+  late final zip_set_file_encryption;
+  late final zip_file_rename;
+  late final zip_delete;
 
-  final zip_get_num_entries = dylib.lookupFunction<ZipGetNumEntryNative, ZipGetNumEntryDart>('zip_get_num_entries');
-  final zip_get_name = dylib.lookupFunction<ZipGetNameNative, ZipGetNameDart>('zip_get_name');
-  final zip_name_locate = dylib.lookupFunction<ZipNameLocateNative, ZipNameLocateDart>('zip_name_locate');
-  final zip_get_error = dylib.lookupFunction<ZipGetErrorNative, ZipGetErrorDart>('zip_get_error');
+  late final zip_get_num_entries;
+  late final zip_get_name;
+  late final zip_name_locate;
+  late final zip_get_error;
 
   final ZIP_EM_AES_128 = 0x0101; // Winzip AES encryption
   final ZIP_EM_AES_192 = 0x0102;
@@ -102,14 +108,52 @@ class ZipArchiver {
 
   final ZIP_FL_ENC_UTF_8 = 2048; // string is UTF-8 encoded
 
-
   late Pointer<Void> _zipFile;
   bool _isOpen = false;
   String? _password;
 
-  ZipArchiver();
+  final DynamicLibrary? _dylib;
+
+  static DynamicLibrary? _loadLibZip() {
+    try {
+      return DynamicLibrary.open(
+        Platform.isMacOS ? "/opt/homebrew/lib/libzip.dylib" :
+        Platform.isLinux ? "libzip.so" :
+        "libzip.dll",
+      );
+    } catch (e) {
+      print('Failed to open libzip: $e');
+      return null;
+    }
+  }
+
+  LibZipArchiver() : _dylib = _loadLibZip() {
+    if(_dylib == null){
+      throw Exception("libzip isn't available.");
+    }
+
+    zip_open = _dylib.lookupFunction<ZipOpenNative, ZipOpenDart>('zip_open');
+    zip_close = _dylib.lookupFunction<ZipCloseNative, ZipCloseDart>('zip_close');
+    zip_source_buffer = _dylib.lookupFunction<ZipSourceBufferNative, ZipSourceBufferDart>('zip_source_buffer');
+    zip_source_free = _dylib.lookupFunction<ZipSourceFreeNative, ZipSourceFreeDart>('zip_source_free');
+
+    zip_file_add = _dylib.lookupFunction<ZipFileAddNative, ZipFileAddDart>('zip_file_add');
+    zip_set_file_encryption = _dylib.lookupFunction<ZipSetFileEncryptionNative, ZipSetFileEncryptionDart>('zip_file_set_encryption');
+    zip_file_rename = _dylib.lookupFunction<ZipRenameNative, ZipRenameDart>('zip_file_rename');
+    zip_delete = _dylib.lookupFunction<ZipDeleteNative, ZipDeleteDart>('zip_delete');
+
+    zip_get_num_entries = _dylib.lookupFunction<ZipGetNumEntryNative, ZipGetNumEntryDart>('zip_get_num_entries');
+    zip_get_name = _dylib.lookupFunction<ZipGetNameNative, ZipGetNameDart>('zip_get_name');
+    zip_name_locate = _dylib.lookupFunction<ZipNameLocateNative, ZipNameLocateDart>('zip_name_locate');
+    zip_get_error = _dylib.lookupFunction<ZipGetErrorNative, ZipGetErrorDart>('zip_get_error');
+  }
 
   void open(String zipPath, [String? password]) {
+    if(_dylib == null){
+      throw Exception("libzip isn't available.");
+      // note that _isOpen is kept to be false.
+    }
+
     _password = password;
     final errorPtr = calloc<Int32>();
     final zipPathNative = zipPath.toNativeUtf8();
@@ -256,5 +300,209 @@ class ZipArchiver {
     }
 
     return fileList;
+  }
+}
+
+
+// --- ZipArchiverExtCmd
+class ZipArchiverExtCmd extends IZipArchiver {
+  Future<void> _printProcessOutput(Process process) async {
+    await for (var line in process.stdout.transform(SystemEncoding().decoder)) {
+      print("stdout: $line");
+    }
+    await for (var line in process.stderr.transform(SystemEncoding().decoder)) {
+      print("stderr: $line");
+    }
+
+    final exitCode = await process.exitCode;
+    print("Process exited with code: $exitCode");
+  }
+
+  late String zipName;
+  String? password;
+  bool _isOpen = false;
+
+  ZipArchiverExtCmd();
+
+  void open(String zipName, [String? password]) {
+    _isOpen = true;
+    this.zipName = zipName;
+    this.password = password;
+  }
+
+  void addFile(String path, String targetPath) async {
+    if (!_isOpen) throw Exception("ZIP file is not open.");
+
+    print("addFile:${path} to ${targetPath}");
+
+    final file = File(path);
+    final dir = Directory(path);
+    final isFile = await file.exists();
+    final parentDir = isFile ? file.parent.path : dir.parent.path;
+    final zipTarget = isFile ? file.uri.pathSegments.last : p.basename(dir.path);
+
+    List<String> args;
+    if( password?.isNotEmpty == true ){
+      args = isFile ? ['-P', password!, zipName, zipTarget] : ['-P', password!, zipName, '-r', zipTarget];
+    } else {
+      args = ['-r', zipName, zipTarget];
+    }
+    
+    print("Running command: zip ${args.join(' ')}");
+
+    final process = await Process.start('zip', args, workingDirectory: parentDir, runInShell: true);
+    await _printProcessOutput(process);
+
+  }
+
+  void rename(String oldName, String newName) {
+    throw Exception("Not implemented.");
+  }
+
+  void renameFolder(int zipPointer, String oldFolder, String newFolder) {
+    throw Exception("Not implemented.");
+  }
+
+  void remove(String fileName) {
+    throw Exception("Not implemented.");
+  }
+
+  void close() {
+    _isOpen = false;
+  }
+}
+
+
+// ZipArchiver (API provider)
+class ZipArchiver extends IZipArchiver {
+  late IZipArchiver impl;
+
+  ZipArchiver(){
+    try{
+      impl = new LibZipArchiver();
+      print("using LibZipArchiver");
+    } catch(e){
+      impl = new ZipArchiverExtCmd();
+      print("fallback mode... using ZipArchiverExtCmd.");
+    }
+  }
+
+  void open(String zipName, [String? password]){
+    impl.open(zipName, password);
+  }
+
+  void addFile(String path, String targetPath) async{
+    impl.addFile(path, targetPath);
+  }
+
+  void rename(String oldName, String newName) {
+    impl.rename(oldName, newName);
+  }
+
+  void renameFolder(int zipPointer, String oldFolder, String newFolder) {
+    impl.renameFolder(zipPointer, oldFolder, newFolder);
+  }
+
+  void remove(String fileName) {
+    impl.remove(fileName);
+  }
+
+  void close() {
+    impl.close();
+  }
+}
+
+
+// helper
+class ZipArchiverHelper
+{
+  static String getDeltaPath(String baseDir, String targetPath) {
+    final String expandedBase = p.absolute(p.normalize(baseDir));
+    final String expandedTarget = p.absolute(p.normalize(targetPath));
+
+    // case : not started with baseDir
+    if (!expandedTarget.startsWith(expandedBase)) {
+      return targetPath;
+    }
+
+    // same path
+    if (expandedTarget.length == expandedBase.length) {
+      return "";
+    }
+
+    final candidate = expandedTarget.substring(expandedBase.length + 1);
+    return candidate.length < targetPath.length ? candidate : targetPath;
+  }
+
+  static void createZipFile(String targetZipFile, List<String> targetFiles, [String? password]) async {
+    // create zip file
+    final zip = ZipArchiver();
+    zip.open(targetZipFile, password);
+    final baseDir = p.dirname(targetZipFile);
+
+    // add file to the zipfile
+    for (var targetFile in targetFiles) {
+      print("zip ${targetFile} to ${targetZipFile}");
+      final file = File(targetFile);
+      final isFile = await file.exists();
+      if( isFile ){
+        zip.addFile(targetFile, getDeltaPath(baseDir, targetFile));
+      }
+    }
+    zip.close();
+  }
+
+  // --- one file case. hoge.txt -> hoge.zip, hoge/ -> hoge.zip
+  static Future<String> getZipFilePath(String path) async {
+    final file = File(path);
+    final dir = Directory(path);
+    final isFile = await file.exists();
+    final parentDir = isFile ? file.parent.path : dir.parent.path;
+    final baseName = isFile ? file.uri.pathSegments.last.split('.').first : p.basename(dir.path);
+    return "$parentDir/$baseName.zip";
+  }
+
+  // --- iterate files
+  static Stream<FileSystemEntity> listEntities(Directory directory) async* {
+    try {
+      await for (final entity in directory.list()) {
+        yield entity;
+      }
+    } catch (e) {
+      print('Error listing directory ${directory.path}: $e');
+    }
+  }
+
+  static Future<void> findFilesRecursively(String targetPath, void Function(File file) onFileFound) async {
+    final directory = Directory(targetPath);
+
+    if (await directory.exists()) {
+      await for (final entity in listEntities(directory)) {
+        if (entity is File) {
+          onFileFound(entity);
+        } else if (entity is Directory) {
+          await findFilesRecursively(entity.path, onFileFound);
+        }
+      }
+    } else {
+      print('not found: ${targetPath}');
+    }
+  }
+
+  // --- convert to files
+  static Future<List<String>> getFileList(List<String> targets) async {
+    List<String> targetFiles = [];
+    for (final path in targets) {
+      final dir = Directory(path);
+      final isDir = await dir.exists();
+      if( isDir ){
+        await findFilesRecursively(path, (File file) {
+          targetFiles.add(file.path);
+        });
+      } else {
+        targetFiles.add(path);
+      }
+    }
+    return targetFiles;
   }
 }
